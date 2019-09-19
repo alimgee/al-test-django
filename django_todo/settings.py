16244b4099e736384e60e7aa08e668f958fef35f
Alan Mc Gee <mcgee.al@gmail.com>
2019-09-19 20:09:04 +0100

    debug setting

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'lh5p-yto(r6cgzjrn+&a2psvy8*6$a2qtjdr1m1*ho3sbs-&yc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
